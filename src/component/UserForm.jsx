6c5d30454fd857fb0daabd2f34fc89d0653445b3
alex <nba4everwanted@gmail.com>
2023-06-10 14:10:57 +0000

    added form

---
+++ src/component/UserForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserForm = () => {
+  return (
+    <div>
+       <form action="">
+         <label>user</label>
+         <input type="text"/><br />
+         <input type="submit" value="Create User" />
+       </form>
+    </div>
+  )
+}
+
+export default UserForm
